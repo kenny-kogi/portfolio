c0fac4572b08c7bb58ec72ddf5bb4e4c270b712d
kenny-kogi <mwangiken497@gmail.com>
2021-08-23 17:12:51 +0300

    finall navbar changes

--- src/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById("burger");
-const ul = document.querySelector("nav ul");
-
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
-});
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Navbar.css";
 import kogi from "../assets/images/kogi.png";
 
 function Navbar() {
+  const [burger, setBurger] = useState(false);
+  const handleToggle = () => {
+    setBurger((prev) => !prev);
+  };
+
   return (
     <div className="navbar-wrapper">
       <img src={kogi} alt="kogi dev" />
-      <button class="burger" id="burger">
-        &#9776
+      <button class="toggle-button" onClick={handleToggle}>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
       </button>
 
-      <ul>
+      <ul className={`${burger ? "button-toggle " : ""}`}>
         <li>
           <a href="#home">Home</a>
         </li>
