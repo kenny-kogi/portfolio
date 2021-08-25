1682b4a53b4eaa813290487cb46bb4b4487e9e6e
kenny-kogi <mwangiken497@gmail.com>
2021-08-26 02:06:59 +0300

    skills section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/skills.css";
+
+function Skills() {
+  return (
+    <div>
+      <h1>Preffered Tools</h1>
+    </div>
+  );
+}
+
+export default Skills;
