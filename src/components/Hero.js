ea639255582615129a0b8d28c8ab363f0fa4a57f
kenny-kogi <mwangiken497@gmail.com>
2021-08-25 03:32:22 +0300

    finall hero section changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,42 +12,44 @@ function Hero() {
   return (
     <div className="main-hero">
       <div className="line-bar"></div>
+      <aside className="side-section">
+        <div className="main-section">
+          <section className="main-details">
+            <h1>Hi</h1>
+            <h1>I'm Keneth kogi</h1>
+          </section>
 
-      <div className="main-section">
-        <section className="main-details">
-          <h1>Hi</h1>
-          <p>I'm Keneth kogi</p>
-        </section>
-
-        <section className="more-details">
-          <p>
-            A passionate techy focusing mainly <br />
-            on web and mobile development based in Nairobi, Kenya
-          </p>
-        </section>
-      </div>
-
-      <div className="contact-links">
-        <div>
-          <a href="https://github.com/kenny-kogi">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a href="https://www.linkedin.com/in/keneth-kogi-mwangi-9732551b1/">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a href="https://twitter.com/kogii_">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a href="https://gmail.com">
-            <FontAwesomeIcon icon={faEnvelopeSquare} />
-          </a>
+          <section className="more-details">
+            <p>
+              A passionate techy focusing mainly <br />
+              on web and mobile development
+              <br /> based in Nairobi, Kenya
+            </p>
+          </section>
         </div>
 
-        <div className="more-buttons">
-          <button>Resume</button>
-          <button>Contact Me</button>
+        <div className="contact-links">
+          <div>
+            <a href="https://github.com/kenny-kogi">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="https://www.linkedin.com/in/keneth-kogi-mwangi-9732551b1/">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="https://twitter.com/kogii_">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a href="https://gmail.com">
+              <FontAwesomeIcon icon={faEnvelopeSquare} />
+            </a>
+          </div>
+
+          <div className="more-buttons">
+            <button>Resume</button>
+            <button>Contact Me</button>
+          </div>
         </div>
-      </div>
+      </aside>
     </div>
   );
 }
