5635898d833899d03771565872384cda7786c270
kenny-kogi <mwangiken497@gmail.com>
2021-08-25 04:04:06 +0300

    about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/css/about.css";
+
+function About() {
+  return (
+    <div className="main-about">
+      <span className="underline">About Me</span>
+      <span></span>
+    </div>
+  );
+}
+
+export default About;
